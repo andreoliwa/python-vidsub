3b82c8042c8f78081fd8d0dea7e64eeb0ffe794e
W. Augusto Andreoli <andreoliwa@gmail.com>
2020-04-16 22:54:20 +0200

    feat: add missing .nfo files

--- movies.py
+++ movies.py
@@ -9,19 +9,22 @@ import imdb
 import re
 import sys
 from subprocess import run, PIPE
-from typing import Tuple, Union
+from typing import Tuple, Union, List, Optional, Any
 
 ROOT_PATH = Path("~/data").expanduser()
 HORROR_MOVIES_PATH = ROOT_PATH / "horror-movies"
 MOVIES_PATH = ROOT_PATH / "movies"
-NOT_MOVIE_SUFFIXES = {".sub"}
+IGNORE_SUFFIXES = {".sub"}
 IMDB_URL = "https://www.imdb.com/title/tt"
 IMDB_SEARCH_URL = "https://www.imdb.com/find?q="
 TORRENT_SEARCH_COMMAND = "torrent-search -a -i on1337x "
+MISSING_TXT = "missing.txt"
+UNIQUE_SEPARATOR = "±"
+PARTIAL_NAME_ARGUMENT = click.argument("partial_name", nargs=-1, required=True)
 
 
 @click.group()
-def movies():
+def main():
     """Tools for movies."""
     pass
 
@@ -35,50 +38,117 @@ def iter_movie_directories(patterns: Tuple[str] = None):
             yield movie_path
 
 
-@movies.command()
-def missing():
+class MovieManager:
+    def __init__(self, verbose: bool):
+        self.ia = imdb.IMDb()
+        self.verbose = verbose
+
+    def format_imdb_url(self, movie: imdb.Movie.Movie) -> str:
+        return f"{IMDB_URL}{movie.movieID}"
+
+    def format_info(self, movie: imdb.Movie.Movie, full=False) -> str:
+        title = movie["title"]
+        year = movie.get("year")
+        url = self.format_imdb_url(movie)
+        if not full:
+            return f"{movie.movieID} {UNIQUE_SEPARATOR} {title} {UNIQUE_SEPARATOR} {year} {UNIQUE_SEPARATOR} {url}"
+
+        if self.verbose:
+            click.echo(f"Loading movie {movie.movieID} from IMDb")
+        movie = self.ia.get_movie(movie.movieID)
+        rating = movie.get("rating", "Not rated")
+        return f"{title} ({year})\nRating: {rating}\n{url}"
+
+    def search_imdb(self, movie_dir: Path) -> Optional[imdb.Movie.Movie]:
+        slugged: str = slugify(movie_dir.name, separator=UNIQUE_SEPARATOR)
+        parts = slugged.split(UNIQUE_SEPARATOR)
+        movies: Optional[imdb.Movie.Movie] = None
+        chosen_line: str = None
+        choices: List[str] = []
+        while parts:
+            movies = self.ia.search_movie(" ".join(parts))
+            if not movies:
+                parts.pop()
+                continue
+
+            choices = [self.format_info(movie) for movie in movies]
+            chosen_line = fzf(choices)
+            if chosen_line:
+                break
+
+            # Nothing was chosen, maybe because none of the options were good.
+            # Remove one part and search again
+            parts.pop()
+
+        for choice in choices:
+            chosen_movie_id = chosen_line.split(UNIQUE_SEPARATOR)[0].strip()
+            for movie in movies:
+                if movie.movieID == chosen_movie_id:
+                    return movie
+        return None
+
+
+@main.command()
+@click.option("--force", "-f", is_flag=True, default=False, help=f"Force creation of {MISSING_TXT}")
+@click.option("--verbose", "-v", is_flag=True, default=False, help=f"Verbose display")
+@PARTIAL_NAME_ARGUMENT
+def missing(force: bool, verbose: bool, partial_name: Tuple[str]):
     """Missing movies (empty folders)."""
-    ia = imdb.IMDb()
+    movie_manager = MovieManager(verbose)
 
-    for movie_path in iter_movie_directories():
-        missing_txt = movie_path / "missing.txt"
+    for movie_dir in iter_movie_directories(partial_name):
+        if verbose:
+            click.echo(f"\nMovie directory: {movie_dir}")
+        missing_txt = movie_dir / MISSING_TXT
 
-        found_movie = False
-        for child_path in movie_path.glob("*"):
+        found_movie = None
+        for child_path in movie_dir.iterdir():
             tags = identify.tags_from_path(child_path)
             if "binary" not in tags:
                 continue
 
             mime_type = magic.from_file(str(child_path), mime=True)
-            if mime_type.startswith("video") and child_path.suffix:
-                # click.echo(f"Found movie {child_path} ({mime_type})")
-                found_movie = True
+            # TODO use identify by default (it should be faster); add --magic option to use optionally
+            if mime_type.startswith("video") and child_path.suffix not in IGNORE_SUFFIXES:
+                if verbose:
+                    click.echo(f"Found movie {child_path.name} ({mime_type})")
+                found_movie = child_path
+                # TODO a dir can have multiple movies; use fzf to select the main one
                 break
 
         if found_movie:
             # Remove the file once a movie is found
             if missing_txt.exists():
                 missing_txt.unlink()
+
+            nfo_file = found_movie.with_suffix(".nfo")
+            # TODO remove .xml files
+            # TODO remove all other .nfo files, keep only this one
+            if nfo_file.exists():
+                stat = nfo_file.stat()
+                click.echo(f"NFO file found: {nfo_file.name} (size in bytes: {stat.st_size})")
+            else:
+                imdb_movie = movie_manager.search_imdb(movie_dir)
+                if imdb_movie:
+                    url = movie_manager.format_imdb_url(imdb_movie)
+                    nfo_file.write_text(f"{url}\n")
+                    if verbose:
+                        click.echo(f"Writing {url} on {nfo_file}")
             continue
 
-        click.secho(f"\n{movie_path}", fg="bright_red", err=True)
-        if missing_txt.exists():
+        click.secho(f"\n{movie_dir}", fg="bright_red", err=True)
+        if not force and missing_txt.exists():
             click.echo(missing_txt.read_text())
             continue
 
-        clean_movie_name = slugify(movie_path.name, separator="+")
         lines = []
+        clean_movie_name = slugify(movie_dir.name, separator="+")
         lines.append(f"{TORRENT_SEARCH_COMMAND}{clean_movie_name}")
         lines.append(f"IMDB Search: {IMDB_SEARCH_URL}{clean_movie_name}")
 
-        results = ia.search_movie(clean_movie_name.replace("+", " "))
-        if results:
-            first_id = results[0].movieID
-            movie = ia.get_movie(first_id)
-            title = movie["title"]
-            year = movie["year"]
-            rating = movie.get("rating", "?")
-            lines.append(f"{title} ({year})\nRating: {rating}\n{IMDB_URL}{first_id}")
+        imdb_movie = movie_manager.search_imdb(movie_dir)
+        if imdb_movie:
+            lines.append(movie_manager.format_info(imdb_movie, full=True))
 
         content = "\n".join(lines)
         missing_txt.write_text(content)
@@ -90,7 +160,7 @@ def ls_movie(path: Union[Path, str]):
     run(f"ls -lh '{path}'", shell=True, universal_newlines=True)
 
 
-@movies.command()
+@main.command()
 @click.argument("partial_name", nargs=-1, required=True)
 def ls(partial_name):
     """List movies by a partial dir name."""
@@ -98,26 +168,30 @@ def ls(partial_name):
         ls_movie(movie_path)
 
 
-@movies.command()
-@click.argument("partial_name", nargs=-1, required=True)
-def rm(partial_name):
+def fzf(items: List[Any]) -> str:
+    choices = "\n".join([str(item) for item in items]).replace("'", "")
+    #  --tac
+    return run(
+        f"echo '{choices}' | fzf --height={len(items) + 2} --cycle", shell=True, stdout=PIPE, universal_newlines=True,
+    ).stdout.strip()
+
+
+def fail(message: str, exit_code: int = 1) -> None:
+    click.secho(message, err=True, fg="bright_red")
+    sys.exit(exit_code)
+
+
+@main.command()
+@PARTIAL_NAME_ARGUMENT
+def rm(partial_name: Tuple[str]):
     """Remove a movie directory by a partial dir name."""
     movie_list = [str(movie_path) for movie_path in iter_movie_directories(partial_name)]
     if not movie_list:
-        click.secho("No movie found", err=True, fg="bright_red")
-        sys.exit(1)
+        fail("No movie found", 1)
 
-    choices = "\n".join(movie_list)
-    chosen_dir = run(
-        f"echo '{choices}' | fzf --height={len(movie_list)} --cycle --tac",
-        shell=True,
-        stdout=PIPE,
-        universal_newlines=True,
-    ).stdout.strip()
-
+    chosen_dir = fzf(movie_list)
     if not chosen_dir:
-        click.secho("No directory selected", err=True, fg="bright_red")
-        sys.exit(2)
+        fail("No directory selected")
 
     ls_movie(chosen_dir)
     click.confirm("\nDo you really want to remove this directory?", abort=True)
@@ -126,7 +200,7 @@ def rm(partial_name):
     click.secho(f"Directory removed: {chosen_dir}", fg="green")
 
 
-@movies.command()
+@main.command()
 def check():
     """Check files on the root dir and other problems."""
     root_files = [str(path) for path in chain(MOVIES_PATH.iterdir(), HORROR_MOVIES_PATH.iterdir()) if not path.is_dir()]
@@ -139,4 +213,4 @@ def check():
 
 
 if __name__ == "__main__":
-    movies()
+    main()
